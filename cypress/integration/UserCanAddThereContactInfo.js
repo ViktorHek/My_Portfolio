861acd4fed7ee6b3c45ef127e62850ec48a63386
viktor <viktorkarlsson125@gmail.com>
2020-11-05 22:16:47 +0100

    ändrat test för ContactForm, test fail. testar i praktiken, funkar inte än men kanske bara behöver vänta

--- cypress/integration/UserCanAddThereContactInfo.js
+++ cypress/integration/UserCanAddThereContactInfo.js
@@ -1,14 +1,21 @@
 describe("User can add there contact information", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000");
-        cy.get("#contactme-tab").click();
     });
 
-    it("display email, name and message", () => {
-        cy.get("#how").should("contain", "name:", "email:", "message:");
-        cy.get(".header").should("contain", "This is where I've been")
-        cy.get(".description").should("contain", "I'm showing this embarrassing excuse of a website as a testament of growth");
+    describe("to contact and it", () => {
 
-    });
+        it("shows your name", () =>{
+            cy.get("#how-ContactForm").should("contain", "Your Name:");
+        })
+
+        it("shows your email", () =>{
+            cy.get("#how-ContactForm").should("contain", "Your Email:");
+        })
+
+        it("shows your email", () =>{
+            cy.get("#how-ContactForm").click();
+        })
+
+    })
 })
-    

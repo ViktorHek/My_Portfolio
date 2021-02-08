ec5f57e951b886d2889ab660d628ed3602fa0f2d
viktor <viktorkarlsson125@gmail.com>
2021-02-08 16:20:45 +0100

    removing githublink for deployment

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,9 +7,9 @@ const ProjectCard = ({ project }) => {
       <Card.Content>
         <h2>{project.name}</h2>
         <Card.Description>{project.description}</Card.Description>
-        <Button icon href={project.link}>
+        {/* <Button icon href={project.link}>
           <Icon name="external alternate" />
-        </Button>
+        </Button> */}
       </Card.Content>
       <Card.Content extra>
         <a>

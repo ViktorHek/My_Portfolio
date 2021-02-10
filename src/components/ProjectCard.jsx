415d9085085cc1f56f2f7280da3f99c186500765
viktor <viktorkarlsson125@gmail.com>
2021-02-10 20:56:20 +0100

    adding links to redos. updating WhoPage-text and styling WhoPage

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Button, Icon } from 'semantic-ui-react'
 
 const ProjectCard = ({ project }) => {
   return (
@@ -7,6 +7,9 @@ const ProjectCard = ({ project }) => {
       <Card.Content>
         <h2>{project.name}</h2>
         <Card.Description>{project.description}</Card.Description>
+        <Button icon href={project.link}>
+          <Icon name="external alternate" />
+        </Button>
       </Card.Content>
       <Card.Content extra>
         <h5>{project.language}</h5>
--- src/components/pages/Who.jsx
+++ src/components/pages/Who.jsx
@@ -1,45 +1,50 @@
-import React from "react";
-import "../../index.css";
+import React from 'react'
+import '../../index.css'
 
 const Who = () => {
   return (
     <>
       <div className="whoContainer">
         <div className="whoText">
-          <h1 id="about-header">Hey! My name is Viktor Karlsson</h1>
+          <h1 id="about-header">Who am I?</h1>
+          <h2 className="whoTextTitle"> Junior Full Stack Web Developer</h2>
           <p>
-            I’m a music nerd graduate in social science, educated in basic
-            leadership, organisation and communications from the gymnasium
-            Marina Läroverket and is currently studying media and communication
-            at Gothenburg University. I’m a former entrepreneur in The Swedish
-            young entrepreneurship (UF) where I was the sales manager. Also I
-            attended management training at STS Alpresor, I have a driving
-            license, a ship commander class VIII license and I’m the team
-            caption of a Swedish football team called IFK Sollentuna.
+            I work with Test Driven Development in an Agil way. I create
+            webpages with React, mobile-applications with Expo/React-Native,
+            APIs with Ruby on Rails. I test my front-end using Cypress and my
+            back-end using RSpec
           </p>
+          <h2 className="whoTextTitle">Don't underestimate a junior</h2>
           <p>
-            My interests in life are sailing, music and philosophy in the likes
-            of Ludwig Wittgenstein, Platon and Siddhartha Gautama. I yearn for
-            knowledge in the hope of one day come as close as possible to
-            understand the fundamental desires of people. The biggest interests
-            currently in philosophy is happiness. What is happiness and can you
-            measure it? Can you only achieve happiness if you desire it ore is
-            it the other way around. Epicurus ones said “Let no one be slow to
-            seek wisdom when he is young nor weary in the search of it when he
-            has grown old. For no age is too early or too late for the health of
-            the soul”. I intend to gain knowledge to seek wisdom.
+            I started building websites after a 12 week Full Stack Web Developer
+            Bootcamp. 12 weeks may not sound like enough time to learn what you
+            need to know to become a web developer. But I advise you who are
+            reading this to check out my projects and think for yourself. "If he
+            managed to learn this in 12 weeks, how much can he learn if he works
+            for us??"
           </p>
+          <h2 className="whoTextTitle">A little about me</h2>
           <p>
-            I have given you my merits and thinking, the only thing left is my
-            process. I always strive to do things different and I have found
-            that very helping in setting me apart from other people. And even
-            when I fail I have at least tried something new and feel a sense of
-            achievement in having fun.
+            I'm working on getting my bachelor's degree in media and
+            communication science. My dream is to become a FULL full-stack web
+            developer sometime in the future. By that, I mean that I want to be
+            able to build frontend and backend applications in combination with
+            having a sufficient understanding of communicative science to know
+            what type of communication to use. As well as build on my knowledge
+            skills in web design to be able to make the website and the message
+            of the website attractive to the user. I strongly believe that
+            functionality, message and style are the cornerstones of a really
+            successful website. With the help of my programming skills, I hope
+            to be able to build websites with all the functionality that the
+            site requires. With knowledge in the science of knowledge, I hope to
+            be able to convey the website's message in a good way. In order not
+            only to get visitors to the website but also to get them to stay
+            there, I think you need to make it aesthetically pleasing.
           </p>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Who;
+export default Who
